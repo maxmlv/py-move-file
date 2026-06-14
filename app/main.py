943f769a88dcdbe534341c717ba453dc3f9a07c7
maxmlv <max.mlv.k@gmail.com>
2026-06-14 19:27:53 +0300

    implement move_file function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    if len(command.split()) == 3:
+        cmd, source, dest = command.split()
+        if cmd != "mv":
+            raise ValueError("Invalid command: command must be 'mv'")
+        if not os.path.exists(source):
+            raise FileNotFoundError(f"{source} does not exist")
+        if source[-1] == "/":
+            raise ValueError(
+                "Invalid command: source file must be a file, not a directory"
+            )
+        if dest[-1] == "/":
+            dest = dest + source
+        if os.path.dirname(dest) != "":
+            os.makedirs(os.path.dirname(dest), exist_ok=True)
+        with open(source, "r") as source_file, open(dest, "w") as dest_file:
+            dest_file.write(source_file.read())
+        os.remove(source)
