8bf90137f1f8715b7148a902d006dba146b6fc00
maxmlv <max.mlv.k@gmail.com>
2026-06-14 19:46:18 +0300

    fix: function follows the logic of mkdir first then mv file

--- app/main.py
+++ app/main.py
@@ -2,20 +2,22 @@ import os
 
 
 def move_file(command: str) -> None:
-    if len(command.split()) == 3:
-        cmd, source, dest = command.split()
-        if cmd != "mv":
-            raise ValueError("Invalid command: command must be 'mv'")
-        if not os.path.exists(source):
-            raise FileNotFoundError(f"{source} does not exist")
-        if source[-1] == "/":
-            raise ValueError(
-                "Invalid command: source file must be a file, not a directory"
-            )
-        if dest[-1] == "/":
-            dest = dest + source
-        if os.path.dirname(dest) != "":
-            os.makedirs(os.path.dirname(dest), exist_ok=True)
-        with open(source, "r") as source_file, open(dest, "w") as dest_file:
-            dest_file.write(source_file.read())
-        os.remove(source)
+    cmd_parts = command.split(" ")
+    if len(cmd_parts) != 3:
+        raise ValueError("Invalid command: must be -> mv <source> <destination>")
+    cmd, source, dest = cmd_parts
+    if cmd != "mv":
+        raise ValueError("Invalid command: command must be 'mv'")
+    if not os.path.exists(source):
+        raise FileNotFoundError(f"{source} does not exist")
+    if not os.path.isfile(source):
+        raise ValueError(
+            "Invalid command: source file must be a file, not a directory"
+        )
+    if dest.endswith("/"):
+        dest = os.path.join(dest, os.path.basename(source))
+    if os.path.dirname(dest):
+        os.makedirs(os.path.dirname(dest), exist_ok=True)
+    with open(source, "r") as source_file, open(dest, "w") as dest_file:
+        dest_file.write(source_file.read())
+    os.remove(source)
